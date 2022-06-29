dcc9e917c8c61237a9398d16a4299aad519ac5fa
Cristiano Roberto da Silva <javacristiano2021@gmail.com>
2022-06-29 06:28:22 -0300

    fim

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -6,12 +6,12 @@ import java.io.IOException;
 
 public class Programa {
 	public static void main(String[] args) {
-		String[] lines = new String[] { "Good morning", "Good afternoon", "Good night" };
+		String[] lines = new String[] { "Good morning", "Good afternoon", "Good night","Bom dia" };
 		String path = "gravar.txt";
 		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
 			for (String line : lines) {
 				bw.write(line);
-				bw.newLine();
+				bw.newLine(); //quebra de linha
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
